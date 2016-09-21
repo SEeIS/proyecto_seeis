33e56c2d52423e73cd270303c73f416c64e1351d
SEeIS_Admin <seeis.esimecu.ipn@gmail.com>
2016-09-20 19:17:16 -0500

    add files .scss

---
+++ sites/all/themes/seeis/template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Implements template_preprocess_html().
+ */
+function seeis_preprocess_html(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_page.
+ */
+function seeis_preprocess_page(&$variables) {
+}
+
+/**
+ * Implements template_preprocess_node.
+ */
+function seeis_preprocess_node(&$variables) {
+}
