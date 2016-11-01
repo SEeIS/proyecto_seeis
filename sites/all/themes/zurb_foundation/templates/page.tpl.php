ae6ca864d45a18d5e606cfc63cb03622aecb3015
SEeIS_Admin <seeis.esimecu.ipn@gmail.com>
2016-10-31 18:20:13 -0600

    Ajustes en estilos y html

--- sites/all/themes/zurb_foundation/templates/page.tpl.php
+++ sites/all/themes/zurb_foundation/templates/page.tpl.php
@@ -140,7 +140,7 @@
 
       <?php if ($action_links): ?>
         <ul class="action-links">
-          <?php print render($action_links); ?>
+          <?php print render($actio\\n_links); ?>
         </ul>
       <?php endif; ?>
 
